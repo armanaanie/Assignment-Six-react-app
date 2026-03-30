907c9dce5c395fa0b4cbfc9905f9b0ecff00abb5
armanaanie <viviandesena96@gmail.com>
2026-03-31 00:04:53 +0600

    one  commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='font-bold text-red-500'>Assignment 6</h1>
+    </>
+  )
+}
+
+export default App
